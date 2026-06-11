5a03cb09a3e53370808d79ffcfc4508133318d82
m00n3r <jeewakasandeep2021@gmail.com>
2026-06-11 19:28:49 +0530

    fix: update environment variable path and key for BFF_PORT

--- bff/src/main.ts
+++ bff/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new GrpcExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(getEnv('PORT', '3001')!);
+  await app.listen(getEnv('BFF_PORT', '3001')!);
 }
 bootstrap();
